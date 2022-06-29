82276c7a831b6da980fed81acb235d90bbe268b5
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2022-06-29 23:40:35 +0530

    Provided json data for guest-book

--- src/app/guestBookHandler.js
+++ src/app/guestBookHandler.js
@@ -21,6 +21,13 @@ const commentsHandler = ({ url, guestBook, saveComments }, res) => {
 };
 
 const guestBookPageCreator = (req, res) => {
+  const { accept } = req.headers;
+  if (accept === 'application/json') {
+    const guestBook = req.guestBook.getComments();
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify(guestBook));
+    return true;
+  }
   res.setHeader('content-type', 'text/html');
   res.end(req.guestBook.toHtml());
   return true;
