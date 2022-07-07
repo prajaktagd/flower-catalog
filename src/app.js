6e7f4539d42b621d7b3afecfc3b6ca3e8ad6e814
Prajakta Dhage <99481665+prajaktagd@users.noreply.github.com>
2022-07-07 13:01:57 +0530

    Implemented login and logout functionalities.
    * Used cookies.

--- src/app.js
+++ src/app.js
@@ -4,19 +4,24 @@ const { notFoundHandler } = require('./app/notFoundHandler.js');
 const { methodNotSupportedHandler } = require('./app/methodNotSupportedHandler.js');
 const { serveStaticFrom } = require('./app/serveStatic.js');
 const { createGuestBookLoader } = require('./app/loadGuestBook.js');
+const { injectCookies } = require('./app/injectCookies.js');
+const { injectSession } = require('./app/injectSession.js');
 
 const guestBookLib = require('./app/guestBookHandlers.js');
 const apiLib = require('./app/apiHandlers.js');
-const { guestBookPageCreator, commentsAdder } = guestBookLib;
+const { guestBookPageCreator, commentsAdder, loginHandler, serveLoginForm, logoutHandler } = guestBookLib;
 const { guestBookApiHandler, guestBookQueryHandler } = apiLib;
 
 const app = (serveFrom) => {
   const commentsFile = './data/comments.json';
   const templateFile = './resources/guest-book-template.html';
+  const sessions = {};
 
   const guestBookHandlers = {
     '/guest-book': { 'GET': guestBookPageCreator },
-    '/guest-book/add-comment': { 'POST': commentsAdder }
+    '/guest-book/add-comment': { 'POST': commentsAdder },
+    '/login': { 'GET': serveLoginForm, 'POST': loginHandler(sessions) },
+    '/logout': { 'GET': logoutHandler(sessions) }
   };
 
   const apiHandlers = {
@@ -32,6 +37,8 @@ const app = (serveFrom) => {
   const serveStatic = serveStaticFrom(serveFrom, aliases);
 
   const handlers = [
+    injectCookies,
+    injectSession(sessions),
     fetchBodyParams,
     loadGuestBook,
     guestBookRouter,
--- src/app/guestBookHandlers.js
+++ src/app/guestBookHandlers.js
@@ -11,10 +11,79 @@ const commentsAdder = ({ bodyParams, guestBook, saveComments }, res) => {
 
   res.statusCode = 302;
   res.setHeader('location', '/guest-book');
-  res.end('');
+  res.end();
 };
 
-const guestBookPageCreator = ({ guestBook, headers }, res) => {
+const loginPage = `<html>
+<head>
+  <title>Login</title>
+</head>
+<body>
+  <form action="/login" method="post">
+    <div>
+      <label>Username: </label>
+      <input type="text" name="username">
+    </div>
+    <div>
+      <input type="submit" value="Submit">
+    </div>
+  </form>
+</body>
+</html>`;
+
+const serveLoginForm = ({ session }, res) => {
+  if (session) {
+    res.statusCode = 302;
+    res.setHeader('location', '/guest-book');
+    res.end();
+    return;
+  }
+  res.setHeader('content-type', 'text/html');
+  res.end(loginPage);
+};
+
+const createSession = (username) => {
+  const time = new Date();
+  return { username, time, sessionId: time.getTime() };
+};
+
+const logoutHandler = (sessions) => ({ session }, res) => {
+  if (session) {
+    const { sessionId } = session;
+    delete sessions[sessionId];
+    res.setHeader('set-cookie', `sessionId=${sessionId};max-Age=0`);
+  }
+  res.statusCode = 302;
+  res.setHeader('location', '/');
+  res.end();
+};
+
+const loginHandler = (sessions) => ({ bodyParams }, res) => {
+  const { username } = getParams(bodyParams);
+
+  if (!username) {
+    res.statusCode = 400;
+    res.setHeader('content-type', 'text/plain');
+    res.end('Provide username');
+    return;
+  }
+
+  const session = createSession(username);
+  sessions[session.sessionId] = session;
+  res.statusCode = 302;
+  res.setHeader('location', '/guest-book');
+  res.setHeader('set-cookie', 'sessionId=' + session.sessionId);
+  res.end();
+};
+
+const guestBookPageCreator = ({ guestBook, headers, session }, res) => {
+  if (!session) {
+    res.statusCode = 302;
+    res.setHeader('location', '/login');
+    res.end();
+    return;
+  }
+
   if (headers.accept === 'application/json') {
     const comments = guestBook.getComments();
     res.setHeader('content-type', 'application/json');
@@ -26,4 +95,4 @@ const guestBookPageCreator = ({ guestBook, headers }, res) => {
   res.end(guestBook.toHtml());
 };
 
-module.exports = { guestBookPageCreator, commentsAdder };
+module.exports = { guestBookPageCreator, commentsAdder, loginHandler, serveLoginForm, logoutHandler };
